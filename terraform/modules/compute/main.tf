ec316f7a9b5d320b1f15eb69f42d6858452e485f
Henrry Alberto Coronado Villca <henrryalbert2016@gmail.com>
2026-05-20 20:29:36 -0400

    feat(evidence): add evidence and fix compute

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -4,21 +4,19 @@ resource "aws_instance" "app_server" {
   iam_instance_profile = var.iam_profile_name
 
   vpc_security_group_ids = [var.security_group_id]
+  user_data_replace_on_change = true
 
   user_data = <<-EOF
               #!/bin/bash
-              # Actualizar paquetes e instalar dependencias con YUM
+              # Actualizacion forzada para recuperar la aplicacion
               yum update -y
               yum install -y git nodejs npm
 
-              # Clonar el repositorio (Reemplaza con el enlace a TU fork)
               git clone https://github.com/henrrycoronado/legacy-inventory.git /home/ec2-user/app
               
-              # Ingresar a la carpeta de la app e instalar dependencias de Node
               cd /home/ec2-user/app
               npm install
 
-              # Configurar el puerto y ejecutar la aplicacion en segundo plano
               export PORT=${var.app_port}
               nohup npm start > /home/ec2-user/app/app.log 2>&1 &
               EOF
